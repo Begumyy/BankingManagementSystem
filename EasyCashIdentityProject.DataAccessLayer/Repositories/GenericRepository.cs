949f4bf3c2735e60e6ffd6cd9502e6db2343b895
Begüm Yünkül <yunkulbegum@gmail.com>
2024-02-21 00:50:46 +0300

    data access katmanına repository ve abstract dosyaları eklendi ve o dosyaların  içine de gerekli olan sınıflar eklendi.

---
+++ EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll() //GetList() methodu >> videoda methodun adı böyle geciyor.
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context= new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+        }
+    }
+}
